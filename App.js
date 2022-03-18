b27dd5125ece9d218791a51b4ac43c2f68ca786b
Walid <walid.mokhtari@etudiant.univ-perp.fr>
2022-03-18 10:13:42 +0100

    FIRST COMMIT

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Login from './src/screens/login';
+
+const App = (props) => {
+  return (
+    <Login />
+  );
+}
+
+export default App;
+
---
+++ src/screens/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Login = (props) => {
+    return (
+        <Text>
+            THIS IS LOGIN PAGE
+        </Text>
+    );
+}
+
+export default Login;
